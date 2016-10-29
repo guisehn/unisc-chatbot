5123362188839e73f412aa3786e08ac76059866e
Guilherme Sehn <ghsehn@gmail.com>
2016-10-29 19:50:56 -0200

    use STX and ETX control characters to control start and end of responses

--- client/client.js
+++ client/client.js
@@ -3,10 +3,10 @@
 const net = require('net')
 const client = new net.Socket()
 const stdin = process.openStdin()
-const argument = (process.argv[2] || '').split(':')
 
-let server = argument[0] ? argument[0] : '0.0.0.0'
-let port = argument[1] ? argument[1] : 1337
+const argument = (process.argv[2] || '').split(':')
+const server = argument[0] ? argument[0] : '0.0.0.0'
+const port = argument[1] ? argument[1] : 1337
 
 console.log(`Conectando em ${server}:${port}`)
 
@@ -17,9 +17,22 @@ client.connect(port, server, () => {
   console.log('\nDigite o comando: ')
 
   client.on('data', (data) => {
-    console.log(`\nServidor:\n${data}`)
-    console.log('\nDigite o comando: ')
-    waiting = false
+    let message = data.toString()
+
+    // detects STX control character to show message start delimiter
+    if (message[0] === `\x02`) {
+      console.log('\nServidor:')
+    }
+
+    // use stdout.write instead of console.log to prevent line breaks
+    // in case message is split into multiple packets
+    process.stdout.write(message)
+
+    // detects ETX control character to release user input
+    if (message[message.length - 1] === '\x03') {
+      waiting = false
+      console.log('\n\nDigite o comando: ')
+    }
   })
 
   stdin.addListener('data', data => {
--- server/server.js
+++ server/server.js
@@ -5,6 +5,34 @@ require('dotenv').config({ silent: true })
 const net = require('net')
 const commands = require('./commands')
 
+// Sends a message for a socket adding STX and ETX control characters
+function send (socket, message, callback) {
+  return socket.write('\x02' + message + '\x03', callback)
+}
+
+// Process command sent by the client and send the response back
+function handleCommand (socket, address, message) {
+  let parts = message.split(' ')
+  let commandName = parts[0].replace('\\', '')
+  let command = commands[commandName]
+
+  if (command) {
+    let argument = parts.splice(1).join(' ')
+
+    command(argument).then(response => {
+      send(socket, response, () => logReturnMessage(message, address))
+    })
+  } else {
+    send(socket, 'Comando não encontrado', () =>
+      console.log(`Servidor não encontrou comando ${commandName} solicitado por ${address}`))
+  }
+}
+
+// Logs internally that a response has been sent to an address
+function logReturnMessage (message, address) {
+  console.log(`Servidor enviou retorno de "${message}" para ${address}`)
+}
+
 const server = net.createServer(socket => {
   let address = `${socket.remoteAddress}:${socket.remotePort}`
 
@@ -18,7 +46,7 @@ const server = net.createServer(socket => {
     if (message.startsWith('\\')) {
       handleCommand(socket, address, message)
     } else {
-      socket.write(message, () => logReturnMessage(message, address))
+      send(socket, message, () => logReturnMessage(message, address))
     }
   })
 
@@ -29,24 +57,3 @@ server.listen(1337, '0.0.0.0', () => {
   let address = server.address()
   console.log(`Servidor iniciado em ${address.address}:${address.port}`)
 })
-
-function handleCommand (socket, address, message) {
-  let parts = message.split(' ')
-  let commandName = parts[0].replace('\\', '')
-  let command = commands[commandName]
-
-  if (command) {
-    let argument = parts.splice(1).join(' ')
-
-    command(argument).then(response => {
-      socket.write(response, () => logReturnMessage(message, address))
-    })
-  } else {
-    socket.write('Comando não encontrado', () =>
-      console.log(`Servidor não encontrou comando ${commandName} solicitado por ${address}`))
-  }
-}
-
-function logReturnMessage (message, address) {
-  console.log(`Servidor enviou retorno de "${message}" para ${address}`)
-}
